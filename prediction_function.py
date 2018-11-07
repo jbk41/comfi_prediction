073ea5fa0535edf839a87e1dfed024c4440507aa
Justin Kim <jbk41@duke.edu>
2018-11-06 22:05:30 -0500

    update requirements and implement linear regressor

---
+++ prediction_function.py
@@ -0,0 +1,69 @@
+import numpy as np
+import json
+from pandas import DataFrame
+from sklearn import linear_model
+
+
+def json_to_data(data_string):
+    parsed_json = json.loads(data_string)
+
+    spend_save = {}
+    spend_save['scaled_monthly_spending_N'] = []
+    spend_save['scaled_monthly_saving_N']   = []
+    spend_save['scaled_monthly_spending_Nm1'] = []
+    spend_save['scaled_monthly_saving_Nm1']   = []
+
+
+    for entry in parsed_json['data']:
+        spend_save['scaled_monthly_spending_N'].append(entry['scaled_monthly_spending'])
+        spend_save['scaled_monthly_saving_N'].append(entry['scaled_monthly_saving'])
+        spend_save['scaled_monthly_spending_Nm1'].append(entry['scaled_monthly_spending'])
+        spend_save['scaled_monthly_saving_Nm1'].append(entry['scaled_monthly_saving'])
+
+    del spend_save['scaled_monthly_spending_Nm1'][-1]
+    del spend_save['scaled_monthly_saving_Nm1'][-1]
+    spend_save['scaled_monthly_spending_N'].pop(0)
+    spend_save['scaled_monthly_saving_N'].pop(0)
+
+    average_monthly_income = parsed_json['average_monthly_income']
+
+    return spend_save
+
+
+
+if __name__ == '__main__':
+
+    import argparse
+    import os
+    import sys
+
+    parser = argparse.ArgumentParser(description='Spend and save predictor')
+    parser.add_argument("data_string",
+                        help="'expecting json data from sim'")
+    args = parser.parse_args()
+
+    df = DataFrame(spend_save,columns=['scaled_monthly_spending_N','scaled_monthly_saving_N','scaled_monthly_spending_Nm1','scaled_monthly_saving_Nm1']) 
+    X = df[['scaled_monthly_spending_Nm1','scaled_monthly_saving_Nm1']] # independent variables
+    save = df['scaled_monthly_saving_N'] # predict savings
+    spend = df['scaled_monthly_spending_N'] # predict spending
+
+    average_spend = np.mean(spend_save_dict['scaled_monthly_spending_N'])
+    average_save = np.mean(spend_save_dict['scaled_monthly_saving_N'])
+
+    save_regr = linear_model.LinearRegression()
+    save_regr.fit(X, save)
+
+    spend_regr = linear_model.LinearRegression()
+    spend_regr.fit(X, spend)
+
+    print('this_month_saving = ' + save_regr.intercept_ + ' + ' + save_regr.coef_[0]  + '*previous_month_spending + ' + save_regr.coef_[1] + '*previous_month_saving')
+    print('this_month_spending = ' + spend_regr.intercept_ + ' + ' + spend_regr.coef_[0]  + '*previous_month_spending + ' + spend_regr.coef_[1] + '*previous_month_saving')
+
+
+
+
+
+
+
+
+
